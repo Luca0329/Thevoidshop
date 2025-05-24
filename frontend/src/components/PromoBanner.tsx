1bd34560775513beef7fdc5f1337a4aad86086b1
Luca0329 <thevoidshop@protonmail.com>
2025-05-24 12:23:48 +0200

    Add PromoBanner component in correct frontend directory

---
+++ frontend/src/components/PromoBanner.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PromoBanner: React.FC = () => {
+  return (
+    <div className="promo-banner">
+      <h2>Special Offer!</h2>
+      <p>Get 20% off on all items this week!</p>
+    </div>
+  );
+};
+
+export { PromoBanner };
+export default PromoBanner;
