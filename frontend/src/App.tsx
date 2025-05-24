e4df9c8823de0c8e3cfa038f38b15348dbddf264
Luca0329 <thevoidshop@protonmail.com>
2025-05-24 22:17:58 +0200

    Use ProductDetailTemp to fix routing - ProductDetail.tsx ignored by gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import HomePage from './pages/HomePage';
 import ApparelPage from './pages/ApparelPage';
 import MusicPage from './pages/MusicPage';
 import AccessoriesPage from './pages/AccessoriesPage';
-import ProductDetailPage from './pages/ProductDetail';
+import ProductDetailTemp from './pages/ProductDetailTemp';
 // import SuccessPage from './pages/SuccessPage';
 
 function App() {
@@ -26,9 +26,8 @@ function App() {
               <Route path="/apparel" element={<ApparelPage />} />
               <Route path="/music" element={<MusicPage />} />
               <Route path="/accessories" element={<AccessoriesPage />} />
-              <Route path="/product/:handle" element={<ProductDetailPage />} />
+              <Route path="/product/:handle" element={<ProductDetailTemp />} />
               {/* <Route path="/success" element={<SuccessPage />} /> */}
-              <Route path="*" element={<div>Page not found</div>} />
             </Routes>
             <Footer />
             <MusicPlayer />
